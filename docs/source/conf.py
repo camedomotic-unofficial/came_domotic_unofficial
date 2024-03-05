efc39aa83ea7ca5159b112ff90b467c0671347dd
Fredericks1982 <fredericks1982@gmail.com>
2024-03-05 15:10:10 +0100

    Remove unused Sphinx extension and update index.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,13 +18,13 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx_rtd_theme",
-    "sphinx.ext.doctest",
     "sphinx.ext.todo",
     "sphinx.ext.coverage",
 ]
 
 templates_path = ["_templates"]
 exclude_patterns = exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
